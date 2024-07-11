4f6bc6db9811dee6a1c97992098a90a21970a2ed
COCO <hong.git@yahoo.com>
2024-07-11 19:50:34 +0900

    Managers 코드 추가.

--- Apple/M1/Assets/@Scripts/Managers/Managers.cs
+++ Apple/M1/Assets/@Scripts/Managers/Managers.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    
+    private static Managers s_instance; // 유일성이 보장된다
+    private static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
+
+    public static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            
+            // 초기화
+            s_instance = go.GetComponent<Managers>();
+            
+        }
+    }
 }
