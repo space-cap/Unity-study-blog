306568a81c6297a493c666b725ef184ec833573a
COCO <hong.git@yahoo.com>
2024-07-11 20:02:24 +0900

    MonoBehaviour 삭제
    Managers 에서 MonoBehaviour 를 가지고 있기 때문.

--- Apple/M1/Assets/@Scripts/Managers/Core/ResourceManager.cs
+++ Apple/M1/Assets/@Scripts/Managers/Core/ResourceManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceManager : MonoBehaviour
+public class ResourceManager
 {
     // Start is called before the first frame update
     void Start()
--- Apple/M1/Assets/@Scripts/Managers/Managers.cs
+++ Apple/M1/Assets/@Scripts/Managers/Managers.cs
@@ -7,6 +7,28 @@ public class Managers : MonoBehaviour
     private static Managers s_instance; // 유일성이 보장된다
     private static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
 
+
+
+    #region Core
+
+    private DataManager _data = new DataManager();
+    private PoolManager _pool = new PoolManager();
+    private ResourceManager _resource = new ResourceManager();
+    private SceneManagerEx _scene = new SceneManagerEx();
+    private SoundManager _sound = new SoundManager();
+    private UIManager _ui = new UIManager();
+
+
+    public static DataManager Data { get { return Instance?._data; } }
+    public static PoolManager Pool { get { return Instance?._pool; } }
+    public static ResourceManager Resource { get { return Instance?._resource; } }
+    public static SceneManagerEx Scene { get { return Instance?._scene; } }
+    public static SoundManager Sound { get { return Instance?._sound; } }
+    public static UIManager UI { get { return Instance?._ui; } }
+
+    #endregion
+
+
     public static void Init()
     {
         if (s_instance == null)
