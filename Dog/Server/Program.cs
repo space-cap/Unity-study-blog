a84a5df24551c39812ac8a342d46f82e7b5faadf
COCO <hong.git@yahoo.com>
2024-07-15 14:41:09 +0900

    캐시 확인 코드

--- Dog/Server/Program.cs
+++ Dog/Server/Program.cs
@@ -4,6 +4,40 @@
     {
         static void Main(string[] args)
         {
+
+            int[,] arr = new int[10000,10000];
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                {
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        arr[y, x] = 1;
+                    }
+                }
+
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x)순서 걸린 시간 {end - now}");
+            }
+
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int x = 0; x < 10000; x++)
+                {
+                    for (int y = 0; y < 10000; y++)
+                    {
+                        arr[x, y] = 1;
+                    }
+                }
+
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y)순서 걸린 시간 {end - now}");
+            }
+
+
+
             Console.WriteLine("Hello, World!");
         }
     }
