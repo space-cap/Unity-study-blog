92997089e1a2e669cf717a2b76045e6ff9923268
COCO <hong.git@yahoo.com>
2024-07-15 14:07:54 +0900

    thread 예제

--- Dog/ServerCore/Program.cs
+++ Dog/ServerCore/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ServerCore
+﻿using System;
+using System.Threading;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Create a new thread
+        Thread myThread = new Thread(new ThreadStart(MyThreadMethod));
+
+        // Start the thread
+        myThread.Start();
+
+        // Do something on the main thread
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Main thread: " + i);
+            Thread.Sleep(100);
+        }
+    }
+
+    static void MyThreadMethod()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("MyThreadMethod: " + i);
+            Thread.Sleep(100);
         }
     }
 }
