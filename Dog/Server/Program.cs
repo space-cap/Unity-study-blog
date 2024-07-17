fa981a3b65060cf43db95db0c7c06adb03376f62
COCO <hong.git@yahoo.com>
2024-07-17 12:08:11 +0900

    servercore 쪽에서 테스트

--- Dog/Server/Program.cs
+++ Dog/Server/Program.cs
@@ -1,60 +1,10 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
-
-namespace Server
+﻿namespace Server
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            // DNS(domain name system)
-            string host = Dns.GetHostName();
-            IPHostEntry ipHost = Dns.GetHostEntry(host);
-            IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
-
-            // 문지기
-            Socket listenSocket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            try
-            {
-                // 문지기 교육
-                listenSocket.Bind(endPoint);
-
-                // 영업 시작
-                // backlog : 최대 대기수
-                listenSocket.Listen(10);
-
-                while (true)
-                {
-                    Console.WriteLine("listening...");
-
-                    // 손님을 입장시킨다.
-                    Socket clientSocket = listenSocket.Accept();
-
-                    // 받는다.
-                    byte[] recvBuffer = new byte[1024];
-                    int recvBytes = clientSocket.Receive(recvBuffer);
-                    string recvData = Encoding.UTF8.GetString(recvBuffer, 0, recvBytes);
-                    Console.WriteLine($"[from client] {recvData}");
-
-                    // 보낸다.
-                    byte[] sendBuffer = Encoding.UTF8.GetBytes("welcome to mmorpg server!");
-                    clientSocket.Send(sendBuffer);
-
-                    // 쫓아낸다.
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                throw;
-            }
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
