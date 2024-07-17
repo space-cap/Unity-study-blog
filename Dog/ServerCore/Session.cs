d24c6f0a42198c0c117bfda4844dae449d299e6f
COCO <hong.git@yahoo.com>
2024-07-17 14:22:57 +0900

    session 파일 생성

---
+++ Dog/ServerCore/Session.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Session
+    {
+        private Socket _socket;
+
+        public void Init(Socket socket)
+        {
+            _socket = socket;
+            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
+            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+
+            RegisterRecv(recvArgs);
+        }
+
+        void Send(byte[] sendBuffer)
+        {
+            _socket.Send(sendBuffer);
+        }
+
+        void Disconnect()
+        {
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        void RegisterRecv(SocketAsyncEventArgs args)
+        {
+            bool panding = _socket.ReceiveAsync(args);
+            if (panding == false)
+            {
+                OnRecvCompleted(null, args);
+            }
+        }
+
+        void OnRecvCompleted(object sender, SocketAsyncEventArgs args)
+        {
+
+            if (args is {BytesTransferred: > 0, SocketError: SocketError.Success})
+            {
+                try
+                {
+                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+                    Console.WriteLine($"[from client] {recvData}");
+                    RegisterRecv(args);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    throw;
+                }
+
+            }
+            else
+            {
+                // todo
+                // disconnection
+            }
+        }
+    }
+}
