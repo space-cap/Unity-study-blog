61d6f5d0db8ba5deb2a14ec0b8578c36b4a3985d
COCO <hong.git@yahoo.com>
2024-07-19 15:48:54 +0900

    PacketFormat 수정

--- Dog/PacketGenerator/PacketFormat.cs
+++ Dog/PacketGenerator/PacketFormat.cs
@@ -12,19 +12,22 @@ namespace PacketGenerator
 using System;
 using System.Collections.Generic;
 
-public class PacketManager
+class PacketManager
 {{
 	#region Singleton
-	static PacketManager _instance = new PacketManager();
-	public static PacketManager Instance {{ get {{ return _instance; }} }}
+	static PacketManager _instance;
+	public static PacketManager Instance
+	{{
+		get
+		{{
+			if (_instance == null)
+				_instance = new PacketManager();
+			return _instance;
+		}}
+	}}
 	#endregion
 
-	PacketManager()
-	{{
-		Register();
-	}}
-
-	Dictionary<ushort, Func<PacketSession, ArraySegment<byte>, IPacket>> _makeFunc = new Dictionary<ushort, Func<PacketSession, ArraySegment<byte>, IPacket>>();
+	Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>> _onRecv = new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>>();
 	Dictionary<ushort, Action<PacketSession, IPacket>> _handler = new Dictionary<ushort, Action<PacketSession, IPacket>>();
 		
 	public void Register()
@@ -32,7 +35,7 @@ public class PacketManager
 {0}
 	}}
 
-	public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer, Action<PacketSession, IPacket> onRecvCallback = null)
+	public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer)
 	{{
 		ushort count = 0;
 
@@ -41,35 +44,24 @@ public class PacketManager
 		ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
 		count += 2;
 
-		Func<PacketSession, ArraySegment<byte>, IPacket> func = null;
-		if (_makeFunc.TryGetValue(id, out func))
-		{{
-			IPacket packet = func.Invoke(session, buffer);
-			if (onRecvCallback != null)
-				onRecvCallback.Invoke(session, packet);
-			else
-				HandlePacket(session, packet);
-		}}
+		Action<PacketSession, ArraySegment<byte>> action = null;
+		if (_onRecv.TryGetValue(id, out action))
+			action.Invoke(session, buffer);
 	}}
 
-	T MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IPacket, new()
+	void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IPacket, new()
 	{{
 		T pkt = new T();
 		pkt.Read(buffer);
-		return pkt;
-	}}
-
-	public void HandlePacket(PacketSession session, IPacket packet)
-	{{
 		Action<PacketSession, IPacket> action = null;
-		if (_handler.TryGetValue(packet.Protocol, out action))
-			action.Invoke(session, packet);
+		if (_handler.TryGetValue(pkt.Protocol, out action))
+			action.Invoke(session, pkt);
 	}}
 }}";
 
         // {0} 패킷 이름
         public static string managerRegisterFormat =
-@"		_makeFunc.Add((ushort)PacketID.{0}, MakePacket<{0}>);
+@"		_onRecv.Add((ushort)PacketID.{0}, MakePacket<{0}>);
 		_handler.Add((ushort)PacketID.{0}, PacketHandler.{0}Handler);";
 
         // {0} 패킷 이름/번호 목록
@@ -86,7 +78,7 @@ public enum PacketID
 	{0}
 }}
 
-public interface IPacket
+interface IPacket
 {{
 	ushort Protocol {{ get; }}
 	void Read(ArraySegment<byte> segment);
@@ -108,7 +100,7 @@ public interface IPacket
         // {3} 멤버 변수 Write
         public static string packetFormat =
 @"
-public class {0} : IPacket
+class {0} : IPacket
 {{
 	{1}
 
@@ -117,6 +109,8 @@ public class {0} : IPacket
 	public void Read(ArraySegment<byte> segment)
 	{{
 		ushort count = 0;
+
+		ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
 		count += sizeof(ushort);
 		count += sizeof(ushort);
 		{2}
@@ -126,14 +120,17 @@ public class {0} : IPacket
 	{{
 		ArraySegment<byte> segment = SendBufferHelper.Open(4096);
 		ushort count = 0;
+		bool success = true;
+
+		Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
 
 		count += sizeof(ushort);
-		Array.Copy(BitConverter.GetBytes((ushort)PacketID.{0}), 0, segment.Array, segment.Offset + count, sizeof(ushort));
+		success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)PacketID.{0});
 		count += sizeof(ushort);
 		{3}
-
-		Array.Copy(BitConverter.GetBytes(count), 0, segment.Array, segment.Offset, sizeof(ushort));
-
+		success &= BitConverter.TryWriteBytes(s, count);
+		if (success == false)
+			return null;
 		return SendBufferHelper.Close(count);
 	}}
 }}
@@ -153,12 +150,12 @@ public class {0} : IPacket
 {{
 	{2}
 
-	public void Read(ArraySegment<byte> segment, ref ushort count)
+	public void Read(ReadOnlySpan<byte> s, ref ushort count)
 	{{
 		{3}
 	}}
 
-	public bool Write(ArraySegment<byte> segment, ref ushort count)
+	public bool Write(Span<byte> s, ref ushort count)
 	{{
 		bool success = true;
 		{4}
@@ -171,7 +168,7 @@ public List<{0}> {1}s = new List<{0}>();";
         // {1} To~ 변수 형식
         // {2} 변수 형식
         public static string readFormat =
-@"this.{0} = BitConverter.{1}(segment.Array, segment.Offset + count);
+@"this.{0} = BitConverter.{1}(s.Slice(count, s.Length - count));
 count += sizeof({2});";
 
         // {0} 변수 이름
@@ -182,28 +179,28 @@ count += sizeof({1});";
 
         // {0} 변수 이름
         public static string readStringFormat =
-@"ushort {0}Len = BitConverter.ToUInt16(segment.Array, segment.Offset + count);
+@"ushort {0}Len = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
 count += sizeof(ushort);
-this.{0} = Encoding.Unicode.GetString(segment.Array, segment.Offset + count, {0}Len);
+this.{0} = Encoding.Unicode.GetString(s.Slice(count, {0}Len));
 count += {0}Len;";
 
         // {0} 리스트 이름 [대문자]
         // {1} 리스트 이름 [소문자]
         public static string readListFormat =
 @"this.{1}s.Clear();
-ushort {1}Len = BitConverter.ToUInt16(segment.Array, segment.Offset + count);
+ushort {1}Len = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
 count += sizeof(ushort);
 for (int i = 0; i < {1}Len; i++)
 {{
 	{0} {1} = new {0}();
-	{1}.Read(segment, ref count);
+	{1}.Read(s, ref count);
 	{1}s.Add({1});
 }}";
 
         // {0} 변수 이름
         // {1} 변수 형식
         public static string writeFormat =
-@"Array.Copy(BitConverter.GetBytes(this.{0}), 0, segment.Array, segment.Offset + count, sizeof({1}));
+@"success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.{0});
 count += sizeof({1});";
 
         // {0} 변수 이름
@@ -215,17 +212,17 @@ count += sizeof({1});";
         // {0} 변수 이름
         public static string writeStringFormat =
 @"ushort {0}Len = (ushort)Encoding.Unicode.GetBytes(this.{0}, 0, this.{0}.Length, segment.Array, segment.Offset + count + sizeof(ushort));
-Array.Copy(BitConverter.GetBytes({0}Len), 0, segment.Array, segment.Offset + count, sizeof(ushort));
+success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), {0}Len);
 count += sizeof(ushort);
 count += {0}Len;";
 
         // {0} 리스트 이름 [대문자]
         // {1} 리스트 이름 [소문자]
         public static string writeListFormat =
-@"Array.Copy(BitConverter.GetBytes((ushort)this.{1}s.Count), 0, segment.Array, segment.Offset + count, sizeof(ushort));
+@"success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)this.{1}s.Count);
 count += sizeof(ushort);
 foreach ({0} {1} in this.{1}s)
-	{1}.Write(segment, ref count);";
+	success &= {1}.Write(s, ref count);";
 
     }
 }
