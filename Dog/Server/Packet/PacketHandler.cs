7ba930b33b0c3204b5b58df018e616316bd63b5b
COCO <hong.git@yahoo.com>
2024-07-20 15:07:39 +0900

    Server 소스 수정

--- Dog/Server/Packet/PacketHandler.cs
+++ Dog/Server/Packet/PacketHandler.cs
@@ -1,19 +1,22 @@
-﻿using ServerCore;
+﻿using Server;
+using ServerCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 class PacketHandler
 {
-    public static void C_PlayerInfoReqHandler(PacketSession session, IPacket packet)
-    {
-        C_PlayerInfoReq p = packet as C_PlayerInfoReq;
+	public static void C_ChatHandler(PacketSession session, IPacket packet)
+	{
+		C_Chat chatPacket = packet as C_Chat;
+		ClientSession clientSession = session as ClientSession;
 
-        Console.WriteLine($"PlayerInfoReq: {p.playerId} {p.name}");
+		if (clientSession.Room == null)
+			return;
 
-        foreach (C_PlayerInfoReq.Skill skill in p.skills)
-        {
-            Console.WriteLine($"Skill({skill.id})({skill.level})({skill.duration})");
-        }
-    }
+		GameRoom room = clientSession.Room;
+		room.Push(
+			() => room.Broadcast(clientSession, chatPacket.chat)
+		);
+	}
 }
