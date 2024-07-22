37af961bf0a52063fc7078f41bc9906c2c07360c
COCO <hong.git@yahoo.com>
2024-07-22 16:58:34 +0900

    GenerateBySideWinder 함수 추가

--- Elephant/Algorithm/Board.cs
+++ Elephant/Algorithm/Board.cs
@@ -29,9 +29,63 @@ namespace Algorithm
             Tile = new TileType[size, size];
             Size = size;
 
-            GenerateByBinaryTree();
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
         }
 
+        void GenerateBySideWinder()
+        {
+            // 일단 길을 다 막아버리는 작업
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        Tile[y, x] = TileType.Wall;
+                    else
+                        Tile[y, x] = TileType.Empty;
+                }
+            }
+
+            
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                int count = 1;
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == Size - 2 && x == Size - 2)
+                        continue;
+
+                    if (y == Size - 2)
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == Size - 2)
+                    {
+                        Tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        Tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 1;
+                    }
+                }
+            }
+        }
 
         void GenerateByBinaryTree()
         {
