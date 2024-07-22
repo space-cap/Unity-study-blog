33e01d0770f2d542917e10dedaebebbb78bf464d
COCO <hong.git@yahoo.com>
2024-07-22 14:48:05 +0900

    뭘 사용을 할까

---
+++ Elephant/Algorithm/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class Board
+    {
+        public int[] _data = new int[25]; // 배열
+        public List<int> _data2 = new List<int>(); // 동적 배열
+        public LinkedList<int> _data3 = new LinkedList<int>(); // 연결 리스트
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
