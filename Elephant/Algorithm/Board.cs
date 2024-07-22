09f9f25226c54848d772a2b2a8b6887feddf0d0b
COCO <hong.git@yahoo.com>
2024-07-22 16:31:45 +0900

    사각형 생성

--- Elephant/Algorithm/Board.cs
+++ Elephant/Algorithm/Board.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,61 @@ namespace Algorithm
 {
     public class Board
     {
-        public int[] _data = new int[25]; // 배열
-        public List<int> _data2 = new List<int>(); // 동적 배열
-        public LinkedList<int> _data3 = new LinkedList<int>(); // 연결 리스트
+        const char CIRCLE = '\u25cf';
+        public TileType[,] Tile { get; private set; }
+        public int Size { get; private set; }
 
-        public void Initialize()
+        public enum TileType
         {
+            Empty,
+            Wall,
+        }
 
+        public void Initialize(int size)
+        {
+            Tile = new TileType[size, size];
+            Size = size;
+
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x==0 || x == Size - 1 || y==0 || y == size-1)
+                        Tile[y, x] = TileType.Wall;
+                    else
+                        Tile[y, x] = TileType.Empty;
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(Tile[y,x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
         }
     }
 }
--- Elephant/Algorithm/Program.cs
+++ Elephant/Algorithm/Program.cs
@@ -4,20 +4,24 @@
     {
         static void Main(string[] args)
         {
+            Board board = new Board();
+            board.Initialize(25);
+
             Console.CursorVisible = false;
             const int WAIT_TICK = 1000 / 30;
-            const char CIRCLE = '\u25cf';
 
             int lastTick = 0;
             while (true)
             {
                 #region 프레임 관리
+
                 // 만약에 경과한 시간이 1/30초보다 작다면
                 int currentTick = System.Environment.TickCount;
                 if (currentTick - lastTick < WAIT_TICK)
                     continue;
                 int deltaTick = currentTick - lastTick;
                 lastTick = currentTick;
+
                 #endregion
 
                 // 입력
@@ -26,19 +30,10 @@
 
                 // 랜드링
                 Console.SetCursorPosition(0, 0);
-
-                for (int i = 0; i < 25; i++)
-                {
-                    for (int j = 0; j < 25; j++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(CIRCLE);
-                    }
-                    Console.WriteLine();
-                }
+                board.Render();
 
             }
-            
+
         }
     }
 }
