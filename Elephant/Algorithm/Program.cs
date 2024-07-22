26ab61fa809fca0e465e4cc6b4befb33072d4522
COCO <hong.git@yahoo.com>
2024-07-22 12:51:13 +0900

    프레임 관리 추가

--- Elephant/Algorithm/Program.cs
+++ Elephant/Algorithm/Program.cs
@@ -5,9 +5,20 @@
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
 
+            int lastTick = 0;
             while (true)
             {
+                #region 프레임 관리
+                // 만약에 경과한 시간이 1/30초보다 작다면
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+                #endregion
+
                 // 입력
 
                 // 로직
