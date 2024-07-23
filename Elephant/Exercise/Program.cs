6ec904ba2c243b76238d071e00d5c03a2944c8ad
COCO <hong.git@yahoo.com>
2024-07-23 14:41:39 +0900

    DFS, BFS

---
+++ Elephant/Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercise
+{
+    class Graph
+    {
+        private int[,] adj = new int[6, 6]
+        {
+            {0, 1, 0, 1, 0, 0},
+            {1, 0, 1, 1, 0, 0},
+            {0, 1, 0, 0, 0, 0},
+            {1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0},
+        };
+
+        private List<int>[] adj2 = new List<int>[]
+        {
+            new List<int>() {1, 2},
+            new List<int>() {0, 2, 3},
+            new List<int>() {1},
+            new List<int>() {0, 1, 4,},
+            new List<int>() {3, 5},
+            new List<int>() {4},
+        };
+    }
+
+
+
+    public class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            // DFS(Depth First Search 깊이 우선 탐색)
+            // BFS(Breadth First Search 너비 우선 탐색)
+
+            
+        }
+    }
+}
