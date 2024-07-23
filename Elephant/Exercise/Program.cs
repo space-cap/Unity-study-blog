8f45d723cb88fd8cbb76db114949665ecef24bfd
COCO <hong.git@yahoo.com>
2024-07-23 15:12:11 +0900

    DFS 구현

--- Elephant/Exercise/Program.cs
+++ Elephant/Exercise/Program.cs
@@ -7,7 +7,7 @@
             {0, 1, 0, 1, 0, 0},
             {1, 0, 1, 1, 0, 0},
             {0, 1, 0, 0, 0, 0},
-            {1, 1, 0, 0, 0, 0},
+            {1, 1, 0, 0, 1, 0},
             {0, 0, 0, 1, 0, 1},
             {0, 0, 0, 0, 1, 0},
         };
@@ -21,22 +21,44 @@
             new List<int>() {3, 5},
             new List<int>() {4},
         };
-    }
 
+        bool[] visited = new bool[6];
+
+        public void DFS(int now)
+        {
+            Console.WriteLine(now);
+            visited[now] = true;
+
+            // 1. 우선 now부터 방문하고
+            // 2. now와 연결된 정점들을 하나씩 확인해서, 아직 미발견(미방문) 상태라면 방문한다.
+            // adj.GetLength(0)
+            for (int next = 0; next < adj.GetLength(0); next++)
+            {
+                if (adj[now, next] == 0)
+                {
+                    continue;
+                }
+
+                if (visited[next])
+                {
+                    continue;
+                }
+
+                DFS(next);
+            }
+        }
+    }
 
 
     public class Program
     {
-
-
-
-
         static void Main(string[] args)
         {
             // DFS(Depth First Search 깊이 우선 탐색)
             // BFS(Breadth First Search 너비 우선 탐색)
 
-            
+            Graph graph = new Graph();
+            graph.DFS(3);
         }
     }
 }
