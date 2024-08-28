79315ccf89ebd69f9f8ba8e633f629b18a409968
space-cap <hong.git@yahoo.com>
2024-08-28 21:09:40 +0900

    managers 수정

--- Ball/MMO_Unity/Assets/Scripts/Managers/Managers.cs
+++ Ball/MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
+    static Managers Instance;
+    static Managers GetInstance() {  return Instance; }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Instance = this; 
+        GameObject go = GameObject.Find("@Managers");
+        Instance = go.GetComponent<Managers>();
+
     }
 
     // Update is called once per frame
