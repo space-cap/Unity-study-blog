150ab6641af88f8e3e3d00a7b00132845c6241a7
space-cap <hong.git@yahoo.com>
2024-08-29 18:21:54 +0900

    좌표 설명

--- Ball/MyMMO/Assets/Scripts/PlayerController.cs
+++ Ball/MyMMO/Assets/Scripts/PlayerController.cs
@@ -17,26 +17,31 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        //transform.TransformDirection
+        // Local -> World
+        // transform.TransformDirection
+
+        // World -> Local
+        // transform.InverseTransformDirection
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
+            //transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
         }
 
     }
