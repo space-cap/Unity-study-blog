b15c3152b08a32b3f107c8ebecbc19806db85bf6
space-cap <hong.git@yahoo.com>
2024-09-02 16:54:46 +0900

    collision

---
+++ Ball/MyMMO/Assets/Scripts/TestCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collider other)
+    {
+        Debug.Log("collision!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger!");
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
