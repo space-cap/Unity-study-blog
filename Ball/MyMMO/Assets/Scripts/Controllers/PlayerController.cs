44a148e8e63d63dbaeb351742573b9116a112856
space-cap <hong.git@yahoo.com>
2024-09-04 17:52:03 +0900

    PlayerController

--- Ball/MyMMO/Assets/Scripts/Controllers/PlayerController.cs
+++ Ball/MyMMO/Assets/Scripts/Controllers/PlayerController.cs
@@ -7,11 +7,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float _speed = 10.0f;
 
+    bool _moveToDest = false;
+    Vector3 _destPos;
+
     // Start is called before the first frame update
     void Start()
     {
         Managers.Input.KeyAction -= OnKeyBoard;
         Managers.Input.KeyAction += OnKeyBoard;
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
 
@@ -21,7 +26,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;
+            if (dir.magnitude < 0.01)
+            {
+                _moveToDest = false;
+            }
+            else 
+            { 
+                float moveDist = Mathf.Clamp( _speed * Time.deltaTime, 0, dir.magnitude );
 
+                transform.position = transform.position + dir.normalized * _speed * Time.deltaTime;
+                transform.LookAt(_destPos);
+            }
+        }
 
     }
 
@@ -52,6 +71,31 @@ public class PlayerController : MonoBehaviour
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.3f);
             transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
+        }
+
+        _moveToDest = false;
+    }
+
+    void OnMouseClicked(Define.MouseEvent e)
+    {
+        if (e != Define.MouseEvent.Click)
+        {
+            return;
+        }
+
+        //Debug.Log("OnMouseClicked");
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100, Color.red, 1);
+        LayerMask mask = LayerMask.GetMask("Wall");
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, mask))
+        {
+            _destPos = hit.point;
+            _moveToDest = true;
+
+            //Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
         }
     }
 }
