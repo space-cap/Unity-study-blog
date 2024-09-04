b54291e56b1efa50c92d0e5028576ceff9635222
space-cap <hong.git@yahoo.com>
2024-09-04 17:12:45 +0900

    CameraController

--- Ball/MyMMO/Assets/Scripts/Controllers/CameraController.cs
+++ Ball/MyMMO/Assets/Scripts/Controllers/CameraController.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField]
+    Define.CamerMode _mode = Define.CamerMode.QuarterView;
+
+    [SerializeField]
+    Vector3 _delta = new Vector3(0,6,-5);
+
+    [SerializeField]
+    GameObject _player = null;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +22,19 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (_mode == Define.CamerMode.QuarterView)
+        {
+            transform.position = _player.transform.position + _delta;
+            transform.LookAt(_player.transform);
+        }
+    }
+
+
+    public void SetQuaterView(Vector3 delta)
+    {
+        _mode = Define.CamerMode.QuarterView;
+        _delta = delta;
     }
 }
---
+++ Ball/MyMMO/Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum CamerMode
+    {
+        None = 0,
+        QuarterView,
+    }
+
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
+}
