0f19af6add6d6aebbdd15e0bcdc380d651a8679e
space-cap <hong.git@yahoo.com>
2024-09-04 15:34:40 +0900

    RaycastHit

--- Ball/MyMMO/Assets/Scripts/TestCollision.cs
+++ Ball/MyMMO/Assets/Scripts/TestCollision.cs
@@ -29,8 +29,23 @@ public class TestCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Local <-> World <-> (Viewport <-> Screen) (화면)
         //Debug.Log(Input.mousePosition);
+        //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
 
-        Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            Vector3 dir = mousePos - Camera.main.transform.position;
+            dir = dir.normalized;
+
+            Debug.DrawRay(Camera.main.transform.position, dir * 100, Color.red, 1);
+
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.transform.position, dir, out hit, 100))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+        }
     }
 }
